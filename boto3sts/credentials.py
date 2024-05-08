5ee4dfa1db3b388f4866faf2a12ca3e9d40e4d43
zinus92 <67749829+zinus92@users.noreply.github.com>
2024-05-08 17:00:05 +0200

    Add RGW (#3)
    * added RGW settings
    
    * Update cygno request rgw credentials.py
    
    ---------
    
    Co-authored-by: Alessandro Costantini <alessandro.costantini@cnaf.infn.it>

--- boto3sts/credentials.py
+++ boto3sts/credentials.py
@@ -6,7 +6,7 @@ import xmltodict
 import liboidcagent as agent
 from boto3 import Session
 
-def s3_session_credentials(oidc_profile, endpoint="https://minio.cloud.infn.it/", verify=True, audience=None, duration_s=3600):
+def s3_session_credentials(oidc_profile, endpoint="https://rgw.cloud.infn.it/", verify=True, audience=object, duration_s=3600):
     if not audience:
         token = agent.get_access_token(
             oidc_profile,
@@ -25,7 +25,8 @@ def s3_session_credentials(oidc_profile, endpoint="https://minio.cloud.infn.it/"
                     'Action':
                     "AssumeRoleWithWebIdentity",
                     'Version': "2011-06-15",
-                    'WebIdentityToken': token
+                    'WebIdentityToken': token,
+                    'RoleArn': 'arn:aws:iam::role/IAMaccess'
                 },
                 verify=verify)
 
@@ -47,7 +48,7 @@ def s3_session_credentials(oidc_profile, endpoint="https://minio.cloud.infn.it/"
         expiry_time=credentials['Expiration'])
 
 
-def assumed_session(oidc_profile, endpoint="https://minio.cloud.infn.it/", verify=True, session=None, audience=None, duration_s=3600):
+def assumed_session(oidc_profile, endpoint="https://rgw.cloud.infn.it/", verify=True, session=None, audience=object, duration_s=3600):
     """STS Role assume a boto3.Session
 
     With automatic credential renewal.
